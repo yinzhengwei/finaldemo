1f132a99940d16a13719cd110095ee39f918cdbb
yinzhengwei@hexindai.com <yinzhengwei@hexindai.com>
2020-08-21 15:14:43 +0800

    功能完善

--- app/src/main/java/cn/com/finaldemo/utils/ToastUtil.kt
+++ app/src/main/java/cn/com/finaldemo/utils/ToastUtil.kt
@@ -1,11 +1,18 @@
 package cn.com.finaldemo.utils
 
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.annotation.SuppressLint
 import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.text.TextUtils
 import android.view.Gravity
 import android.view.LayoutInflater
+import android.view.View
+import android.view.animation.AccelerateInterpolator
+import android.view.animation.LinearInterpolator
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import cn.com.finaldemo.R
@@ -41,4 +48,72 @@ object ToastUtil {
         toast?.show()
     }
 
+    fun showToast_Text(text: String) {
+        showText(text, 20)
+    }
+
+    fun showToast_Text(text: String, footSize: Int) {
+        showText(text, footSize)
+    }
+
+    fun cancelToast() {
+        if (toast != null) {
+            toast?.cancel()
+            toast = null
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    private fun showText(text: String, footSize: Int) {
+        cancelToast()
+        toast = Toast(GmConstans.mInstance)
+        toast?.setGravity(Gravity.CENTER, 0, 0)
+        val view = LayoutInflater.from(GmConstans.mInstance)
+            .inflate(R.layout.layout_sign_toast, null)
+        val layout_sign = view.findViewById<View>(R.id.layout_sign) as RelativeLayout
+        val textView = view.findViewById<View>(R.id.tv_sign) as TextView
+        textView.textSize = footSize.toFloat()
+        textView.text = text
+        toast?.view = view
+        toast?.duration = 3000
+        toast?.show()
+
+        val alpha = ObjectAnimator.ofFloat(layout_sign, "alpha", 0f, 1f)
+        alpha.duration = 300
+        alpha.interpolator = AccelerateInterpolator()
+        alpha.start()
+
+        val scaleX = ObjectAnimator.ofFloat(
+            layout_sign,
+            "scaleX",
+            1f,
+            1.2f,
+            1.5f,
+            1.2f,
+            1f,
+            1.2f,
+            1.5f,
+            1.2f,
+            1f
+        )
+        val scaleY = ObjectAnimator.ofFloat(
+            layout_sign,
+            "scaleY",
+            1f,
+            1.2f,
+            1.5f,
+            1.2f,
+            1f,
+            1.2f,
+            1.5f,
+            1.2f,
+            1f
+        )
+        val set = AnimatorSet()
+        set.duration = 700
+        alpha.interpolator = LinearInterpolator()
+        set.playTogether(scaleX, scaleY)
+        set.startDelay = 300
+        set.start()
+    }
 }
